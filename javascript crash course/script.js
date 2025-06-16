3efab17a161fbe6926ce0e7004355a75561f5421
zeredogo <zeredogo@gmail.com>
2025-06-16 23:46:08 +0100

    loops

---
+++ javascript crash course/script.js
@@ -0,0 +1,12 @@
+// Loops
+// DRY = Don't Repeat Yourself
+let counter = 1
+while (counter <= 20) {
+    console.log(counter)
+    counter++;
+ }
+ console.log("while loop finished running")
+
+ for (let i = 0; i <= 7; i++ ) {
+    console.log(i)
+ }
