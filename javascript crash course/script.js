445d72912148575c999fc1c5c88d17cac5220e8c
zeredogo <zeredogo@gmail.com>
2025-06-17 02:17:27 +0100

    for loop

--- javascript crash course/script.js
+++ javascript crash course/script.js
@@ -1,12 +1,22 @@
 // Loops
 // DRY = Don't Repeat Yourself
 let counter = 1
-while (counter <= 20) {
+while (counter <= 5) {
     console.log(counter)
     counter++;
  }
  console.log("while loop finished running")
 
- for (let i = 0; i <= 7; i++ ) {
-    console.log(i)
+//  for (let i = 1; i <= 20; i++) {
+//     if(i % 5 === 0) {
+//         console.log(`${i} - ASAP Frontend`)
+//     } else if (i % 2 === 0) {
+//         console.log(`${i} - Frontend`)
+//     }else {
+//         console.log(`${i} - ASAP`)
+//     }
+//  }
+let str = "ASAP frontend";
+ for ( let i = 0; i <= str.length; i++) {
+    console.log(str[i])
  }
